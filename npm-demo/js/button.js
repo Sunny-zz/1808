612ad2f0378e92df68cf4711ed9a0f8d09375ddb
Sunny-zz <b695340402@163.com>
2018-12-05 15:31:00 +0800

    npm-demo

---
+++ npm-demo/js/button.js
@@ -0,0 +1,7 @@
+const a = 50
+const b = 500
+
+module.exports = {
+  a,
+  b
+}
---
+++ npm-demo/js/index.js
@@ -0,0 +1,9 @@
+const b = require('./button.js')
+// 导入自定义模块的方法 require('路径')
+// 导入第三方的模块和核心模块 require('模块名')
+const $ = require('jquery')
+console.log($)
+console.log(b)
+const a = 10
+
+console.log(a)
