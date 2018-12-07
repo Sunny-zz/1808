25671677e449ec4952f2ef219cc43f51c4bba316
Sunny-zz <b695340402@163.com>
2018-12-07 16:47:06 +0800

    react-props update

--- react-props/src/components/Button/Button.js
+++ react-props/src/components/Button/Button.js
@@ -3,10 +3,11 @@ import './buttton.css'
 class Button extends Component {
   // 子组件如何接收父组件传递过来的 props
   // 在 render 方法下 直接获取 this.props 就是父组件传递给子组件的 props 对象
+  //
   render() {
     // console.log(this.props)
     const { txt } = this.props
-    return <button className="btn">{txt}</button>
+    return <button className="btn">{txt ? txt : '按钮'}</button>
   }
 }
 
