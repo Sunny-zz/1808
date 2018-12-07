c5015b119082bb5287843249bd53e94604f30171
Sunny-zz <b695340402@163.com>
2018-12-07 15:30:01 +0800

    react-state update 1

--- react-state/src/App.js
+++ react-state/src/App.js
@@ -1,16 +1,58 @@
 import React, { Component } from 'react'
-
+import './app.css'
 class App extends Component {
   // 相当于给该类(组件)定义了一个属性 state 属性值为对象
   state = {
     // 各种状态
-    num: 1
+    num: 1,
+    bg: true,
+    show: false
   }
   render() {
     return (
       <>
         <span>{this.state.num}</span>
         <button onClick={this.fun}>+</button>
+        <div
+          // className={this.state.bg ? 'box active' : 'box'}
+          className={`box ${this.state.bg && 'active'}`}
+          // style={{
+          //   width: '200px',
+          //   height: '200px',
+          //   backgroundColor: this.state.bg ? 'pink' : 'teal'
+          // }}
+        />
+        <button className="btn" onClick={this.changeBg}>
+          change
+        </button>
+        <button
+          className="modal-btn"
+          onClick={() => {
+            this.changeModal(true)
+          }}
+        >
+          Modal
+        </button>
+        <div
+          className="modal-wrap"
+          style={{ display: this.state.show ? 'block' : 'none' }}
+          onClick={() => {
+            this.changeModal(false)
+          }}
+        />
+        <div
+          className="modal"
+          style={{ display: this.state.show ? 'block' : 'none' }}
+        >
+          <button
+            className="cancle"
+            onClick={() => {
+              this.changeModal(false)
+            }}
+          >
+            取消
+          </button>
+        </div>
       </>
     )
   }
@@ -23,6 +65,36 @@ class App extends Component {
     // 不可以直接对 state 进行修改
     this.setState({
       num: this.state.num + 1
+    })
+  }
+  changeBg = () => {
+    // const oldColor = this.state.color
+    // if (oldColor === 'pink') {
+    //   this.setState({
+    //     color: 'teal'
+    //   })
+    // } else {
+    //   this.setState({
+    //     color: 'pink'
+    //   })
+    // }
+    this.setState({
+      bg: !this.state.bg
+    })
+  }
+  // showModal = () => {
+  //   this.setState({
+  //     show: true
+  //   })
+  // }
+  // closeModal = () => {
+  //   this.setState({
+  //     show: false
+  //   })
+  // }
+  changeModal = isShow => {
+    this.setState({
+      show: isShow
     })
   }
 }
