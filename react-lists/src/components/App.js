4edca4b64871734e88c5ff9bb3e1a14bd2fd08f8
Sunny-zz <b695340402@163.com>
2018-12-10 16:37:39 +0800

    react-lists lists

--- react-lists/src/components/App.js
+++ react-lists/src/components/App.js
@@ -4,8 +4,16 @@ class App extends Component {
   state = {
     comments: [
       {
-        id: 1,
+        id: '12312',
         txt: 'hahaha'
+      },
+      {
+        id: '12112',
+        txt: 'hahaha1'
+      },
+      {
+        id: '31232',
+        txt: 'hahaha2'
       }
     ]
   }
@@ -14,7 +22,18 @@ class App extends Component {
     // return 内就是负责展示的，逻辑操作最好在 return 之外
     const { comments } = this.state
     //没有评论的时候展示某个div，有评论的时候展示评论列表
-    const showDiv = comments.length ? <div>展示列表</div> : <div>请评论</div>
+    // 列表渲染 只需要将对象数组变成标签数组
+    //  map 将一个数组生成一个新数组  map 参数内的返回值就是新数组中的元素
+    //  通过标签数组生成的元素 必须要给每一个元素加上属性 key 并且属性值是唯一的 通常 key 的值是数据的 id 值
+    const showDiv = comments.length ? (
+      <ul>
+        {comments.map(comment => (
+          <li key={comment.id}>{comment.txt}</li>
+        ))}
+      </ul>
+    ) : (
+      <div>请评论</div>
+    )
 
     return (
       <div>
