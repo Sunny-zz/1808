bb09079c9c3a39c74dd22d7257f4a71ef2e2e3d4
Sunny-zz <b695340402@163.com>
2018-12-10 17:02:16 +0800

    react-lists reverse

--- react-lists/src/components/App.js
+++ react-lists/src/components/App.js
@@ -25,24 +25,38 @@ class App extends Component {
     // 列表渲染 只需要将对象数组变成标签数组
     //  map 将一个数组生成一个新数组  map 参数内的返回值就是新数组中的元素
     //  通过标签数组生成的元素 必须要给每一个元素加上属性 key 并且属性值是唯一的 通常 key 的值是数据的 id 值
+    // 不变性
+    // 直接 comments.reverse  comments 本身也会颠倒顺序，间接的修改了 state 违背了不变性。
+    // 数组的 复制   slice(0)      Object.assign      ...
+
     const showDiv = comments.length ? (
       <ul>
-        {comments.map(comment => (
-          <li key={comment.id}>{comment.txt}</li>
+        {[...comments].reverse().map(comment => (
+          <li key={comment.id}>
+            {comment.txt} <button>删除</button>
+          </li>
         ))}
       </ul>
     ) : (
       <div>请评论</div>
     )
-
+    console.log(this.state.comments)
     return (
       <div>
         <h1>文章</h1>
         <p>asdg asjkdg kasjgd aksjdg ka</p>
+        <textarea name="" id="" cols="30" rows="10" />
+        <button>评论</button>
         <h4>回复</h4>
         {showDiv}
       </div>
     )
+  }
+  xxx = () => {
+    // const newComment = {
+    //   id: '111111',
+    //   txt: 'asdasdas'
+    // }
   }
 }
 
