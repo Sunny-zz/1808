f4e479f8f1782afc957a0729a06fceb51720fb17
Sunny-zz <b695340402@163.com>
2018-12-12 17:17:07 +0800

    react-posts ajax

--- react-posts/src/components/Posts/Posts.js
+++ react-posts/src/components/Posts/Posts.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react'
-
+import axios from 'axios'
 class Posts extends Component {
   state = {
-    posts: [
-      {
-        id: 1,
-        title: 'react 基础'
-      },
-      {
-        id: 2,
-        title: 'vue 基础'
-      }
-    ]
+    posts: []
   }
+  componentDidMount() {
+    // 该生命周期函数 用于刷新页面发送请求获取数据更改 state
+    axios.get('http://localhost:3008/posts').then(res => {
+      this.setState({
+        posts: res.data
+      })
+    })
+  }
+
   render() {
     const { posts } = this.state
-    const list = (
+    const list = posts.length ? (
       <ul>
         {posts.map(post => (
           <li key={post.id}>
@@ -23,6 +23,8 @@ class Posts extends Component {
           </li>
         ))}
       </ul>
+    ) : (
+      <div>请稍等.....</div>
     )
     return <div>{list}</div>
   }
