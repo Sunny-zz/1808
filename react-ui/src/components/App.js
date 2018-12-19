545e631cf05b4f5ff99053c6edb527d519ae55b9
Sunny-zz <1348369672@qq.com>
2018-12-19 11:43:21 +0800

    react-ui recharts update

--- react-ui/src/components/App.js
+++ react-ui/src/components/App.js
@@ -24,6 +24,24 @@ class App extends Component {
         name: 'ff',
         uv: 10
       }
+    ],
+    data1: [
+      {
+        name: '4k-6k',
+        per: '3.9%'
+      },
+      {
+        name: '6k-8k',
+        per: '20%'
+      },
+      {
+        name: '8k-10k',
+        per: '50%'
+      },
+      {
+        name: '10k-15k',
+        per: '16.1%'
+      }
     ]
   }
   render() {
@@ -36,7 +54,13 @@ class App extends Component {
           data={data}
           margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
         >
-          <Line type='monotone' dataKey='uv' stroke='#8884d8' />
+          <Line
+            type='monotone'
+            dataKey='uv'
+            stroke='#8884d8'
+            activeDot={{ r: 10 }}
+            isAnimationActive={false}
+          />
           <CartesianGrid stroke='#ccc' strokeDasharray='5 5' />
           <XAxis dataKey='name' />
           <YAxis />
