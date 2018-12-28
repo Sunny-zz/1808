b3b4f3245ab961bb2e7ae63e06d25dec68d1a650
Sunny-zz <1348369672@qq.com>
2018-12-28 11:52:32 +0800

    shopping-cart total

--- react-redux-cart/src/components/ShoppingCart.js
+++ react-redux-cart/src/components/ShoppingCart.js
@@ -11,7 +11,7 @@ class ShoppingCart extends Component {
             const product = products.find(product => product.id === e)
             return (
               <li key={e}>
-                {product.productName}
+                {product.productName}{' '}
                 <span>
                   {product.price} x <span>{cart.quantityById[e]}</span>
                 </span>
@@ -22,11 +22,21 @@ class ShoppingCart extends Component {
       ) : (
         <div>Please add some products to cart.</div>
       )
+
+    const total =
+      cart.productId.length && products.length
+        ? cart.productId.reduce(
+            (num, e) =>
+              num + products.find(i => i.id === e).price * cart.quantityById[e],
+            0
+          )
+        : 0
+
     return (
       <div>
         <h2>shopping-cart</h2>
         {cartContent}
-        <span>$total 0.00</span>
+        <span>$total {total}</span>
         <br />
         <button disabled={cart.length ? false : true}>Checkout</button>
       </div>
