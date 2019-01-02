9ca36d8f47adf1f198d3a52fd386bfbcb733adc7
Sunny-zz <1348369672@qq.com>
2019-01-02 11:34:45 +0800

    better-scroll start

--- react-redux-mt/src/components/Order.js
+++ react-redux-mt/src/components/Order.js
@@ -1,35 +1,50 @@
 import React, { Component } from 'react'
+import './Order.scss'
+import Bscroll from 'better-scroll'
 
 class Order extends Component {
   componentDidMount() {
     this.props.getGoods()
   }
+  // cdu 更新完毕   props 或者 state 更新之后页面重新渲染完毕才执行
+  componentDidUpdate() {
+    const { goods } = this.props
+    if (goods.length) {
+      // 定义一个全局变量 Bscroll
+      this.Bscroll = new Bscroll(this.foodListWrapper)
+    }
+  }
+
   render() {
     const { goods } = this.props
     console.log(goods)
-
     const goodsContent = goods.length ? (
-      <div>
+      <div className='goods'>
         <div className='food-tabs'>
           {goods.map(e => (
             <div key={e.id}>{e.name}</div>
           ))}
         </div>
-        <div>
-          {goods.map(e => (
-            <div key={e.id}>
-              <h3>{e.name}</h3>
-              {e.foods.map(e => (
-                <div key={e.id}>
-                  <img src={e.icon} alt='' />
-                  <div>
-                    <h4>{e.name}</h4>
-                    <span>{e.price}</span>
+        <div
+          className='food-list-wrapper'
+          ref={listWrapper => (this.foodListWrapper = listWrapper)}
+        >
+          <div className='food-list'>
+            {goods.map(e => (
+              <div key={e.id}>
+                <h3>{e.name}</h3>
+                {e.foods.map(e => (
+                  <div className='food' key={e.id}>
+                    <img src={e.icon} alt='' />
+                    <div className='food-info'>
+                      <h4>{e.name}</h4>
+                      <span>{e.price}</span>
+                    </div>
                   </div>
-                </div>
-              ))}
-            </div>
-          ))}
+                ))}
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     ) : (
