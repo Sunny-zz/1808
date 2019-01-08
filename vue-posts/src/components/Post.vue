20550496e49f179826afa41f9907701a889051d5
Sunny-zz <1348369672@qq.com>
2019-01-08 16:40:22 +0800

    vue-posts done

--- vue-posts/src/components/Post.vue
+++ vue-posts/src/components/Post.vue
@@ -5,6 +5,14 @@
       <p>{{post.body}}</p>
     </div>
     <div v-else>请稍等。。。。</div>
+    <div>
+      <input v-model="commentTxt" type="text">
+      <button @click="addComment">评论</button>
+      <ul v-if="comments.length">
+        <li v-for="comment in reverseComments" :key="comment.id">{{comment.txt}}</li>
+      </ul>
+      <div v-else>评论为空</div>
+    </div>
   </div>
 </template>
 <script>
@@ -13,8 +21,17 @@ import axios from "axios";
 export default {
   name: "post",
   data: () => ({
-    post: null
+    post: null,
+    comments: [],
+    commentTxt: ""
   }),
+  computed: {
+    // 计算的数据
+    // computed 对象下的每一个属性都是一个计算数据，值要设置成一个 普通函数，并且需要返回一个值，这个返回的值就提供给组件使用。
+    reverseComments() {
+      return [...this.comments].reverse();
+    }
+  },
   created() {
     // 请求数据
     // 整个项目的路由 我们创建的 router.js
@@ -25,6 +42,22 @@ export default {
     axios.get(`http://localhost:3008/posts/${id}`).then(res => {
       this.post = res.data;
     });
+    axios.get(`http://localhost:3008/comments?postId=${id}`).then(res => {
+      this.comments = res.data;
+    });
+  },
+  methods: {
+    addComment() {
+      const { id } = this.$route.params;
+      const newComment = {
+        txt: this.commentTxt,
+        postId: id
+      };
+      axios.post(`http://localhost:3008/comments`, newComment).then(res => {
+        this.comments.push(res.data);
+        this.commentTxt = "";
+      });
+    }
   }
   // mounted() {
   //   // 组件在页面渲染完毕
