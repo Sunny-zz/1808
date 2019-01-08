63aac997f15203faa10b228fb96fedfbbbeeb29c
Sunny-zz <1348369672@qq.com>
2019-01-08 15:15:11 +0800

    vue-posts json

--- vue-posts/src/components/Home.vue
+++ vue-posts/src/components/Home.vue
@@ -12,18 +12,7 @@
 export default {
   name: "home",
   data: () => ({
-    posts: [
-      {
-        id: 1,
-        title: "vue",
-        body: "vue 好 vue 棒"
-      },
-      {
-        id: 2,
-        title: "react",
-        body: "react 牛"
-      }
-    ]
+    posts: []
   })
 };
 </script>
--- vue-posts/src/components/Post.vue
+++ vue-posts/src/components/Post.vue
@@ -1,8 +1,23 @@
 <template>
-  <div>post</div>
+  <div>
+    post
+    <span>{{$route.params.id}}</span>
+  </div>
 </template>
 <script>
+// 路由的 props
 export default {
   name: "post"
+  // created() {
+  //   // 请求数据
+  //   // 整个项目的路由 我们创建的 router.js
+  //   console.log(this.$router);
+  //   // 当前组件接收到的 路由 props
+  //   console.log(this.$route);
+  // },
+  // mounted() {
+  //   // 组件在页面渲染完毕
+  //   // 操作 dom 节点
+  // }
 };
 </script>
