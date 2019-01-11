f4bc17e5f75cb9991fb4e6b78af7404b2d9051dd
Sunny-zz <1348369672@qq.com>
2019-01-11 11:57:56 +0800

    vue-juejin 简化getPosts

--- vue-juejin/src/components/PostList.vue
+++ vue-juejin/src/components/PostList.vue
@@ -1,8 +1,38 @@
 <template>
-  <div>list{{$route.params.type}}</div>
+  <div v-if="posts.length">
+    <ul>
+      <li v-for="post in posts" :key="post.id">
+        <router-link :to="`/post/${post.id}`">{{post.title}}</router-link>
+      </li>
+    </ul>
+  </div>
+  <div v-else>请稍等...</div>
 </template>
 <script>
+import axios from "axios";
 export default {
-  name: "postlist"
+  name: "postlist",
+  created() {
+    this.getPosts("recommend");
+  },
+  data: () => ({
+    posts: []
+  }),
+  beforeUpdate() {
+    console.log(this.path);
+    // this.getPosts();
+  },
+  computed: {
+    path() {
+      return this.$route.path;
+    }
+  },
+  methods: {
+    getPosts(tab) {
+      axios.get(`http://localhost:3008/posts?tab=${tab}`).then(res => {
+        this.posts = res.data;
+      });
+    }
+  }
 };
 </script>
