a65d1a863115cdaa6de63a04971400ffeb4e7fee
Sunny-zz <1348369672@qq.com>
2019-02-22 11:56:09 +0800

    vuex-cart vue transition

--- vuex-cart/src/App.vue
+++ vuex-cart/src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
-    <h1>Vuex cart</h1>
+    <button @click="show = !show">toggle</button>
+    <transition appear name="fade">
+      <h1 v-show="show">Vuex Cart</h1>
+    </transition>
+    <transition appear name="fadeInLeft">
+      <h2>Vue</h2>
+    </transition>
     <Products/>
     <hr>
     <Cart/>
@@ -12,6 +18,9 @@ import Products from "./components/Products";
 import Cart from "./components/Cart";
 export default {
   name: "app",
+  data: () => ({
+    show: true
+  }),
   components: {
     Products,
     Cart
@@ -20,4 +29,28 @@ export default {
 </script>
 
 <style>
+.fade-leave-active,
+.fade-enter-active {
+  /* 定义进入或离开的过渡的效果，时间，速度曲线 */
+  transition: all 1.5s;
+}
+.fade-leave,
+.fade-enter-to {
+  /* 离开的开始样式 ,进入结束的样式*/
+}
+.fade-leave-to,
+.fade-enter {
+  /* 离开结束的样式，进入开始的样式 */
+  transform: translateX(100px);
+  opacity: 0;
+}
+.fadeInLeft-enter {
+  transform: translateX(-100px);
+  opacity: 0;
+}
+
+.fadeInLeft-enter-active {
+  /* 定义进入或离开的过渡的效果，时间，速度曲线 */
+  transition: all 1.5s;
+}
 </style>
