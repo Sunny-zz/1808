2a6f9398435abe4a231c8a0d54cea658a5acede2
Sunny-zz <1348369672@qq.com>
2019-02-22 09:58:11 +0800

    vuex-cart addtocart object

--- vuex-cart/src/store/cart.js
+++ vuex-cart/src/store/cart.js
@@ -9,7 +9,9 @@ const cart = {
         state.cartProductsId.push(productId)
         state.quantityById[productId] = 1
       } else {
+        // 当改变对象下的某个属性时，直接进行改变 vue 认为没有修改，因为修改的都是同一个对象指向的地址是相同的，那么就要对对象进行重新赋值
         state.quantityById[productId]++
+        state.quantityById = { ...state.quantityById }
       }
     }
   },
